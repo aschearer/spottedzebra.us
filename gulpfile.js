575b33abb5cc79a4124c23277975624f1ece91dc
Alex Schearer <Alex Schearer>
2016-12-28 10:25:56 -0800

    Updating deployment, build

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 var gulp = require("gulp");
-var exec = require('child_process').exec;
+var gutil = require('gulp-util'); 
+var exec = require('gulp-exec');
 
 // Compile the site using jekyll
 gulp.task("default", function() {
-     exec('jekyll build --config _config.yml', function(err, stdtmp, stderr) {
-         if (err)
-         {
-             console.log(err.message);
-         }
-         
-         console.log(stdtmp);
-     });
+    return gulp.src('_config.yml')
+        .pipe(exec('jekyll build --config "<%= file.path %>"'))
+        .pipe(exec.reporter());
+});
+
+gulp.task("deploy", function() {
+    return gulp.src('deploy.yaml')
+        .pipe(exec('"tools/stout-windows.exe" deploy --key ' + gutil.env.AWS_KEY + ' --secret ' + gutil.env.AWS_SECRET))
+        .pipe(exec.reporter());
 });
